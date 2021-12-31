15c8041b384e9b7593f7224fdb5429a45f35ee2a
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2021-12-31 10:08:51 -0800

    enforce data volume

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ ! -d "/data" ]; then
+	echo "ERROR: You need to include the data volume when you run this container."
+	echo "Example: --volume eero-client-data:/data"
+	exit 1
+fi
+
 if [ -f "/data/session.cookie" ]; then
 	cp "/data/session.cookie" ./session.cookie
 else
